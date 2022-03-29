80e079d587b9874fe425edc73f44e83e8ca00ac7
Sohel <90556891+sohel22z@users.noreply.github.com>
2022-03-29 17:08:37 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,13 @@ function index() {
             <Container maxWidth="sm" className='triangle-left'></Container>
             <Container fixed>
                 <Box mt={8} sx={{ textAlign: 'center' }}>
-                        <h1 className='bold-header-text' sx={{ my:5 }} >Creating great IT Services & Software solutions for enterprises & startups! </h1>
+                        <h1 className='bold-header-text' sx={{ mb:5 }} >Creating great IT Services & Software solutions for enterprises & startups! </h1>
                         <p className='normal-text' component="div"> A company based in Belgium, Germany. Eureca Apps strives to redefine your mobile app experience and deliver the best
                         products in a fast pacing world with the best passionate people in our team. </p>
                 </Box>
 
                 <Grid container my={2} spacing={3}>
-                    <Grid item xs={12} sm={6}>
+                    <Grid item xs={12} sm={6} >
                         <Card sx={{ maxWidth: 500, float: 'right' }}>
                             <CardActionArea>
                                 <CardMedia
