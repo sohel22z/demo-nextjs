d42c59414ad1b50b0dfd931043711075ad7807b8
Sohel <90556891+sohel22z@users.noreply.github.com>
2022-03-29 16:56:52 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,8 @@ function index() {
             <Container maxWidth="sm" className='triangle-left'></Container>
             <Container fixed>
                 <Box mt={8} sx={{ textAlign: 'center' }}>
-                        <h1 className='bold-header-text'> Creating great IT Services & Software solutions for enterprises & startups! </h6>
+        
+                        <h1 className='bold-header-text'> Creating great IT Services & Software solutions for enterprises & startups! </h1>
 
                     <Typography className='normal-text' variant="subtitle1" color="text.secondary" component="div" gutterBottom>
                         A company based in Belgium, Germany. Eureca Apps strives to redefine your mobile app experience and deliver the best
