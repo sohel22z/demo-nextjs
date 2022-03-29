32cf0d35a9b1937aef352047c39d69ad32447e84
Sohel <90556891+sohel22z@users.noreply.github.com>
2022-03-29 16:46:52 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,8 @@ export default function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <title>IT Services & Software Solution – EurecaApps</title>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <script defer src="https://cdn.jsdelivr.net/npm/lazyload@2.0.0-rc.2/lazyload.js"></script>
             </Head>
